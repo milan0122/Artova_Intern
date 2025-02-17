73c1e0aa0931e875cfd97857606785075766a1d7
Milan <dangi.milan46@gmail.com>
2025-02-17 13:53:05 +0545

    from day1 to day7

---
+++ Django_Task/internship_project/internship_project/urls.py
@@ -0,0 +1,31 @@
+"""
+URL configuration for internship_project project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.contrib import admin
+from django.urls import path
+from tasks.views import Hello,Task_list,create_task,delete_task,task_detail,edit_task
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("hello/",Hello,name='Hello'),
+    path("tasks/",Task_list,name='task_list'),
+    path("tasks/create",create_task,name='create_tasks'),
+    path("task/<int:task_id>/",task_detail,name='task_detail'),
+    path('task/<int:task_id>/delete/',delete_task,name='delete_task'),
+    path('task/<int:task_id>/edit/',edit_task,name='edit_task'),
+
+]
---
+++ Django_Task/internship_project/tasks/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render,redirect, get_object_or_404
+
+# Create your views here.
+from django.http import HttpResponse
+from .models import Task
+from .forms import taskForm
+def Hello(request):
+    return HttpResponse("Welcome to the Internship Program")
+def Task_list(request):
+    tasks = Task.objects.all()
+    return render(request,'tasks/task_list.html',{'tasks':tasks})
+
+def create_task(request):
+    form = taskForm()
+    if request.method == 'POST':
+        form = taskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')    
+    return render(request,'tasks/create_task.html',{'form':form})
+
+def task_detail(request,task_id):
+    task = get_object_or_404(Task,id=task_id)
+    return render(request,'tasks/task_detail.html',{'task':task})
+
+def delete_task(request,task_id):
+    task = get_object_or_404(Task,id=task_id)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('task_list')
+    return render(request,'tasks/delete_task.html',{'task':task})
+
+def edit_task(request,task_id):
+    task = get_object_or_404(Task,id=task_id)
+    if request.method == 'POST':
+        form = taskForm(request.POST,instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('task_detail',task_id=task.id)
+        else:
+            form = taskForm(instance=task)
+    return render(request,'tasks/edit_task.html',{'form':form,'task':task})
